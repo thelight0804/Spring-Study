fc341f501486717f5cc6ce12894ce49da06749a7
thelight0804 <thelight0804@gmail.com>
2023-03-25 16:44:03 +0900

    Use JDBC from JdbcTemplate
    스프링 JdbcTemplate
    DB를 JdbcTemplate을 이용하도록 했습니다

--- Spring-Introduction/src/main/java/thelight0804/SpringIntroduction/SpringConfig.java
+++ Spring-Introduction/src/main/java/thelight0804/SpringIntroduction/SpringConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import thelight0804.SpringIntroduction.repository.JDBCMemberRepository;
+import thelight0804.SpringIntroduction.repository.JDBCTemplateMemberRepository;
 import thelight0804.SpringIntroduction.repository.MemberRepository;
 import thelight0804.SpringIntroduction.repository.MemoryMemberRepository;
 import thelight0804.SpringIntroduction.service.MemberService;
@@ -27,6 +28,7 @@ public class SpringConfig {
   @Bean
   public MemberRepository memberRepository() {
 //    return new MemoryMemberRepository();
-    return new JDBCMemberRepository(dataSource);
+//    return new JDBCMemberRepository(dataSource);
+    return new JDBCTemplateMemberRepository(dataSource);
   }
 }
---
+++ Spring-Introduction/src/main/java/thelight0804/SpringIntroduction/repository/JDBCTemplateMemberRepository.java
@@ -0,0 +1,66 @@
+package thelight0804.SpringIntroduction.repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import thelight0804.SpringIntroduction.domain.Member;
+
+public class JDBCTemplateMemberRepository implements MemberRepository{
+
+  public final JdbcTemplate jdbcTemplate;
+
+  @Autowired
+  public JDBCTemplateMemberRepository(DataSource dataSource) {
+    jdbcTemplate = new JdbcTemplate(dataSource);
+  }
+
+  @Override
+  public Member save(Member member) {
+    //sql insert문 생성
+    SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+    jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("name", member.getName());
+
+    Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+    member.setId(key.longValue());
+    return member;
+  }
+
+  @Override
+  public Optional<Member> findById(Long id) {
+    List<Member> result = jdbcTemplate.query("select * from member where id = ?", memberRowMapper(), id);
+    return result.stream().findAny();
+  }
+
+  @Override
+  public Optional<Member> findByName(String name) {
+    List<Member> result = jdbcTemplate.query("select * from member where name = ?", memberRowMapper(), name);
+    return result.stream().findAny();
+  }
+
+  @Override
+  public List<Member> findAll() {
+    //list로 반환
+    return jdbcTemplate.query("select * from member", memberRowMapper());
+  }
+
+  private RowMapper<Member> memberRowMapper() {
+    return (rs, rowNum) -> {
+      Member member = new Member();
+      member.setId(rs.getLong("id"));
+      member.setName(rs.getString("name"));
+      return member;
+    };
+  }
+}
+
+
