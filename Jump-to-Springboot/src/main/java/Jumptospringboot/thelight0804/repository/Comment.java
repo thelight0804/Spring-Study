5315738200b330621a6e36122bd8473f8735c12b
thelight0804 <thelight0804@gmail.com>
2023-05-13 11:07:46 +0900

    Add song, comment entity
    데이터베이스에 사용할 음악과 댓글 엔티티를 만들었습니다

---
+++ Jump-to-Springboot/src/main/java/Jumptospringboot/thelight0804/repository/Comment.java
@@ -0,0 +1,29 @@
+package Jumptospringboot.thelight0804.repository;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Comment {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column(length = 200)
+  private String content;
+
+  private LocalDateTime createDate;
+
+  @ManyToOne
+  private Song song;
+}
---
+++ Jump-to-Springboot/src/main/java/Jumptospringboot/thelight0804/repository/Song.java
@@ -0,0 +1,34 @@
+package Jumptospringboot.thelight0804.repository;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Song {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column(length = 200)
+  private String title;
+
+  @Column(columnDefinition = "TEXT")
+  private String detail;
+
+  private LocalDateTime createDate;
+
+  @OneToMany(mappedBy = "song", cascade = CascadeType.REMOVE)
+  private List<Comment> commentList;
+}
