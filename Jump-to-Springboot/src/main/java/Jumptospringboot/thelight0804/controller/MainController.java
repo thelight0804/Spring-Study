157d564530c3623cc1adbc4bb983340e6d5c5027
thelight0804 <thelight0804@gmail.com>
2023-05-18 08:55:35 +0900

    Add root page for implement song list
    메인(root)페이지에 음악 리스트가 출력됩니다

--- Jump-to-Springboot/src/main/java/Jumptospringboot/thelight0804/controller/MainController.java
+++ Jump-to-Springboot/src/main/java/Jumptospringboot/thelight0804/controller/MainController.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller //Class를 스프링부트의 Controller로 사용한다
 public class MainController {
 
-  @GetMapping("/main") //URL 요청이 들어오면 main 메서드를 실행한다
-  @ResponseBody //HTTP 바디에 담는다
-  public String main(){
-    return "Now is main";
+  @GetMapping("/") //URL 요청이 들어오면 main 메서드를 실행한다
+  public String root() {
+    return "redirect:/song/list";
   }
 }
