f644ea26164c482c3912596e780777f08e973d07
thelight0804 <thelight0804@gmail.com>
2023-06-12 09:28:28 +0900

    Implement modify comment
    댓글을 수정하는 기능을 구현했습니다

--- Jump-to-Springboot/src/main/java/Jumptospringboot/thelight0804/service/CommentService.java
+++ Jump-to-Springboot/src/main/java/Jumptospringboot/thelight0804/service/CommentService.java
@@ -1,10 +1,12 @@
 package Jumptospringboot.thelight0804.service;
 
+import Jumptospringboot.thelight0804.DataNotFoundException;
 import Jumptospringboot.thelight0804.domain.Comment;
 import Jumptospringboot.thelight0804.domain.SiteUser;
 import Jumptospringboot.thelight0804.domain.Song;
 import Jumptospringboot.thelight0804.repository.CommentRepository;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,23 @@ public class CommentService {
     comment.setCreateDate(LocalDateTime.now());
     comment.setSong(song);
     comment.setAuthor(author);
+    this.commentRepository.save(comment);
+  }
+
+  //조회
+  public Comment getComment(Integer id) {
+    Optional<Comment> comment = this.commentRepository.findById(id);
+    if (comment.isPresent()) {
+      return comment.get();
+    } else{
+      throw new DataNotFoundException("Comment not found");
+    }
+  }
+
+  //수정
+  public void modify(Comment comment, String content) {
+    comment.setContent(content);
+    comment.setUpdateDate(LocalDateTime.now());
     this.commentRepository.save(comment);
   }
 }
